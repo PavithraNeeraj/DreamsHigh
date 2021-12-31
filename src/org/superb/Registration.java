e3883e2c638c939175b5891754131778a2e8a0bf
PavithraNeeraj <pavithraneeraj.golla@gmail.com>
2021-12-31 23:03:29 +0530

    UserNameFunctionality

--- src/org/superb/Registration.java
+++ src/org/superb/Registration.java
@@ -1,5 +1,12 @@
 package org.superb;
 
 public class Registration {
+public static void username() {
+	System.out.println("username");
 
+}
+public static void main(String[] args) {
+	username();
+	
+}
 }
