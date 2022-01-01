17b45a29810c78e722a11ef8925ab90c584c6964
PavithraNeeraj <pavithraneeraj.golla@gmail.com>
2022-01-01 09:37:52 +0530

    Password Functionality

--- src/org/superb/Registration.java
+++ src/org/superb/Registration.java
@@ -1,5 +1,12 @@
 package org.superb;
 
 public class Registration {
+public static void password() {
+	System.out.println("Password");
 
+}
+public static void main(String[] args) {
+	password();
+	
+}
 }
